9f66f0820071d8cef74c9d2898d4f71698944660
Kleyton Ferreira <kleytondev40@gmail.com>
2025-10-24 15:23:50 -0300

    feat: update shadcn theme colors

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Button } from './components/ui/button'
 const App = () => {
   return (
     <div>
-      <Button>Hellow wolrd</Button>
+      <Button className="bg-primary-blue">Hellow wolrd</Button>
     </div>
   )
 }
