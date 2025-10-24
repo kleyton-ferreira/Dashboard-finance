d0358f5821eac6f4057dd786a0a8688e82fb806a
Kleyton Ferreira <kleytondev40@gmail.com>
2025-10-24 13:48:39 -0300

    chore: add shadcn setup

--- eslint.config.js
+++ eslint.config.js
@@ -34,10 +34,6 @@ export default [
       'react/jsx-no-target-blank': 'off',
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
     },
   },
   eslintConfigPrettier,
