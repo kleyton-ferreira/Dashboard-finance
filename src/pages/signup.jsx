8c531d601133bf1a0e8ae66a14db8e97035e80fc
Kleyton Ferreira <kleytondev40@gmail.com>
2025-11-04 14:59:05 -0300

    feat: ensure password and password confirmation are the same on signup form

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -28,32 +28,37 @@ import { useMutation } from '@tanstack/react-query'
 import { api } from '@/lib/axios'
 import { toast } from 'sonner'
 
-const signupSchema = z.object({
-  firstName: z.string().trim().min(1, {
-    message: 'O nome é obrigatório.',
-  }),
-  lastName: z.string().trim().min(1, {
-    message: 'O sobrenome é obrigatório.',
-  }),
-  email: z
-    .string()
-    .email({
-      message: 'O e-mail é inválido',
-    })
-    .trim()
-    .min(1, {
-      message: 'O e-mail é obrigatório',
+const signupSchema = z
+  .object({
+    firstName: z.string().trim().min(1, {
+      message: 'O nome é obrigatório.',
     }),
-  password: z.string().trim().min(6, {
-    message: 'A senha deve ter no mínimo 6 caracteres.',
-  }),
-  passwordConfirmation: z.string().trim().min(6, {
-    message: 'A confirmação de senha é obrigatória.',
-  }),
-  terms: z.boolean().refine((value) => value === true, {
-    message: 'Você precisa aceitar os termos.',
-  }),
-})
+    lastName: z.string().trim().min(1, {
+      message: 'O sobrenome é obrigatório.',
+    }),
+    email: z
+      .string()
+      .email({
+        message: 'O e-mail é inválido',
+      })
+      .trim()
+      .min(1, {
+        message: 'O e-mail é obrigatório',
+      }),
+    password: z.string().trim().min(6, {
+      message: 'A senha deve ter no mínimo 6 caracteres.',
+    }),
+    passwordConfirmation: z.string().trim().min(6, {
+      message: 'A confirmação de senha é obrigatória.',
+    }),
+    terms: z.boolean().refine((value) => value === true, {
+      message: 'Você precisa aceitar os termos.',
+    }),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: 'As senhas não coincidem.',
+    path: ['passwordConfirmation'],
+  })
 
 const SignUp = () => {
   const [user, setUser] = useState(null)
