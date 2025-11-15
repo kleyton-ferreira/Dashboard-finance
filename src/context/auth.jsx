1933ce1c2b6c268e65b5819b4732dadd039c01fd
Kleyton Ferreira <kleytondev40@gmail.com>
2025-11-14 21:14:32 -0300

    refactor: ensure user properties are returned from service on cameCase

--- src/context/auth.jsx
+++ src/context/auth.jsx
@@ -43,6 +43,8 @@ export const AuthContextProvider = ({ children }) => {
     },
   })
 
+  console.log({ user })
+
   useEffect(() => {
     const init = async () => {
       try {
--- src/services/user.js
+++ src/services/user.js
@@ -8,17 +8,35 @@ export const UserService = {
       email: input.email,
       password: input.password,
     })
-    return response.data
+    return {
+      id: response.data.id,
+      email: response.data.email,
+      firstName: response.data.first_name,
+      lastName: response.data.last_name,
+      tokens: response.data.tokens,
+    }
   },
   login: async (input) => {
     const response = await publicApi.post('/users/login', {
       email: input.email,
       password: input.password,
     })
-    return response.data
+    return {
+      id: response.data.id,
+      email: response.data.email,
+      firstName: response.data.first_name,
+      lastName: response.data.last_name,
+      tokens: response.data.tokens,
+    }
   },
   me: async () => {
     const response = await protectedApi.get('/users/me')
-    return response.data
+    console.log(response.data)
+    return {
+      id: response.data.id,
+      email: response.data.email,
+      firstName: response.data.first_name,
+      lastName: response.data.last_name,
+    }
   },
 }
